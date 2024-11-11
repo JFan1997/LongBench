27c96d921acbeb9ccaba30f29b07f7b9a2de03fe
Jack <yyuan4@nd.edu>
2024-11-10 22:05:02 -0500

    Change requirements.txt, add configs, comment out gemma specifications (might not need them)

--- pred.py
+++ pred.py
@@ -40,9 +40,12 @@ def parse_args(args=None):
             "chatglm2-6b-32k",
             "chatglm3-6b-32k",
             "vicuna-v1.5-7b-16k",
+            "phi-1.5",
             "phi-2",
             "gemma-2b",
             "gemma-2-2b",
+            "llama3.2-1b",
+            "llama3.2-3b",
         ],
     )
     parser.add_argument("--e", action="store_true", help="Evaluate on LB-E")
@@ -73,6 +76,8 @@ def build_chat(tokenizer, prompt, model_name):
         prompt = header + f" ### Human: {prompt}\n###"
     elif "internlm" in model_name:
         prompt = f"<|User|>:{prompt}<eoh>\n<|Bot|>:"
+    # elif "gemma" in model_name:
+    #     prompt = f"<start_of_turn>user\n{prompt}<end_of_turn>\n<start_of_turn>model\n"
     return prompt
 
 
@@ -81,6 +86,8 @@ def post_process(response, model_name):
         response = response.strip().replace("Assistant:", "")
     elif "internlm" in model_name:
         response = response.split("<eoa>")[0]
+    # elif "gemma" in model_name:
+    #     response = response.split("<end_of_turn>")[0]
     return response
 
 
@@ -193,22 +200,30 @@ def load_model_and_tokenizer(path, model_name, device, apply_se):
         or "internlm" in model_name
         or "xgen" in model_name
         or "phi" in model_name
+        or "gemma" in model_name
     ):
         tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
         model = AutoModelForCausalLM.from_pretrained(
             path, trust_remote_code=True, torch_dtype=torch.bfloat16
         ).to(device)
-    elif "gemma" in model_name:
-        tokenizer = GemmaTokenizer.from_pretrained(path, trust_remote_code=True)
-        model = GemmaForCausalLM.from_pretrained(
-            path, trust_remote_code=True, torch_dtype=torch.float16
-        ).to(device)
+    # elif "gemma" in model_name:
+    #     tokenizer = GemmaTokenizer.from_pretrained(
+    #         path,
+    #         trust_remote_code=True,
+    #         torch_dtype=torch.float16,  # NOTE: bfloat16 causes error
+    #     )
+    #     model = GemmaForCausalLM.from_pretrained(
+    #         path,
+    #         trust_remote_code=True,
+    #         torch_dtype=torch.float16,  # NOTE: bfloat16 causes error
+    #     ).to(device)
     elif "llama2" in model_name:
         replace_llama_attn_with_flash_attn()
         tokenizer = LlamaTokenizer.from_pretrained(path)
-        model = LlamaForCausalLM.from_pretrained(path, torch_dtype=torch.bfloat16).to(
-            device
-        )
+        model = LlamaForCausalLM.from_pretrained(
+            path,
+            torch_dtype=torch.bfloat16,
+        ).to(device)
     elif "longchat" in model_name or "vicuna" in model_name:
         from fastchat.model import load_model
 
